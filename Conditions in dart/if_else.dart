05b6da2bb526d42aa23a08cdbb2fa381703fe1c4
Admin <salitha.2019347@iit.ac.lk>
2021-08-05 11:49:28 +0530

    Done

---
+++ Conditions in dart/if_else.dart
@@ -0,0 +1,14 @@
+void main(){
+  int a =50;
+  if(a >= 75){
+    print("Distintion Pass");
+  }else if(a<=74 && a>=65 ){
+    print("Very Good Pass");
+  }else if(a<=64 && a>=55){
+    print("Good pass");
+  }else if(a<=54 && a>=35){
+    print("Simple Pass");
+  }else{
+    print ("Fail");
+  }
+}
---
+++ Conditions in dart/nested_if.dart
@@ -0,0 +1,17 @@
+void main(){
+  var a=75;
+  var b=45;
+  var c=95;
+
+  if(a>b){
+    if(a>c){
+      print("a is greater");
+    }else {
+      print("c is greater");
+    }
+  }else if(b>c){
+     print("b is greater");
+  }else{
+    print("c is greater");
+  }
+}
